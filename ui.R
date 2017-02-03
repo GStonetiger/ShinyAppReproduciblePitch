dc5f63ac9752ae49e397a513de42693b16eb78c7
Rick Wang <rwang@lmi.org>
2017-02-02 20:48:18 -0500

    Add minor changes.

--- ui.R
+++ ui.R
@@ -1,12 +1,9 @@
 library(ggplot2)
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  # Application title
-  titlePanel("Normal vs T Distribution"),
-  
-  # Sidebar with a slider input for number of bins
+  titlePanel("t vs Z Distribution"),
+
   sidebarLayout(
     sidebarPanel(
       "The t distribution, invented by William Gosset in 1908, has thicker tails than the normal. Also, instead of having two parameters, mean and variance, as the normal does, the t distribution has only one - the number of degrees of freedom (df).",
@@ -19,8 +16,6 @@ shinyUI(fluidPage(
         value = 3
       )
     ),
-    
-    # Show a plot of the generated distribution
     mainPanel(plotOutput("ddistPlot"),
               plotOutput("qdistPlot"))
   )
